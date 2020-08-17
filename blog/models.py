17ffc72581a939e30bbbd98a432143f06d6095b0
adriano <adrianlimeira@hotmail.com>
2020-08-17 19:04:29 -0300

    Alteracao modelo Post

--- blog/models.py
+++ blog/models.py
@@ -3,9 +3,24 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 
 class Post(models.Model):
+    STATUS = (
+        ('racunho', 'Rascunho'),
+        ('publicado', 'Publicado'),
+    )
     titulo = models.CharField(max_length=255)
     slug = models.SlugField(max_length=255)
     autor = models.ForeignKey(User, on_delete=models.CASCADE)
     conteudo = models.TextField(blank=True)
+
+    publicado = models.DateTimeField(default=timezone.now)
+    criado = models.DateTimeField(auto_now_add=True)
+    alterado = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=10, choices=STATUS, default='rascunho')
+
+    class Meta:
+        ordering = ('publicado',)
+
+    def __str__(self):
+        return self.titulo
     
 
